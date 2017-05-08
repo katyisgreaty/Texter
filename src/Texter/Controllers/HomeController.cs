782ae08d40d628a84e86018d8b22757b3b7ae8f3
Shruti Priya <shrutipriya1808@gmail.com>
2017-05-08 11:17:43 -0700

    setup basic files and ensure security by creating environmentvariables file

---
+++ src/Texter/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Texter.Models;
+
+
+namespace Texter.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //To view all the messages: it will be a standard GET request.
+        public IActionResult GetMessages()
+        {
+            var allMessages = Message.GetMessages();
+            return View(allMessages);
+        } 
+
+        //To Send a message:  this will be a POST request.
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message newMessage)
+        {
+            newMessage.Send();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Texter.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "ACad57e78560296f8759b89a2b8366bed5";
+        public static string AuthToken = "be41a3e4d55288d7c7fdb80cd5fa6fec";
+    }
+}
